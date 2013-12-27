2a5a9cceea54b76cae3c3edfb4c6cc8b0fd6b8ef
Raitis Stengrevics <dagrevis@gmail.com>
2013-12-27 20:39:26 +0200

    Add recursion (this was much harder yet more fun as well)

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,29 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (empty? coll)
+    []
+    (conj (recursive-reverse (rest coll)) (first coll))
+  ))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         x 1]
+    (if (zero? n)
+      x
+      (recur (dec n) (* x n))
+    )
+  ))
 
 (meditations
   "Recursion ends with a base case"
